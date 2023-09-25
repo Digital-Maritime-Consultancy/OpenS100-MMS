c26f09fc6892e31f1ec2e6b32f45f80c9e9dd1dd
Jinki Jung <your.jinki.jung@gmail.com>
2023-09-25 19:27:49 +0200

    MMSAgent - just framing

--- OpenS100/MmsAgent.cpp
+++ OpenS100/MmsAgent.cpp
@@ -10,18 +10,96 @@ CMMSAgent::~CMMSAgent() {
     
 }
 
-bool CMMSAgent::disconnect() {
-    webSocket.stop();
-    return true;
+ResponseEnum CMMSAgent::connectAuthenticated(const std::string& url)
+{
+    return ResponseEnum();
 }
 
-bool CMMSAgent::connect(const std::string& url) {
+AgentState CMMSAgent::status()
+{
+    return AgentState();
+}
+
+ResponseEnum CMMSAgent::connectAnonymous(const std::string& url)
+{
+    return ResponseEnum();
+}
+
+ResponseEnum CMMSAgent::authenticate()
+{
+    return ResponseEnum();
+}
+
+ResponseEnum CMMSAgent::disconnect()
+{
+    return ResponseEnum();
+}
+
+ResponseEnum CMMSAgent::send(const std::string& mrnOrSubject, const bool isBroadcast, const std::vector<uint8_t>& data)
+{
+    // Send a message to the server (default to TEXT mode)
+    webSocket.send("hello world");
+
+    // The message can be sent in BINARY mode (useful if you send MsgPack data for example)
+    webSocket.sendBinary("some serialized binary data");
+    return ResponseEnum();
+}
+
+ResponseEnum CMMSAgent::receive()
+{
+    return ResponseEnum();
+}
+
+ResponseEnum CMMSAgent::subscribe(const std::string& subject)
+{
+    return ResponseEnum();
+}
+
+ResponseEnum CMMSAgent::unsubscribe(const std::string& message)
+{
+    return ResponseEnum();
+}
+
+void CMMSAgent::disconnectWS() {
+    webSocket.stop();
+}
+
+MmtpMessage CMMSAgent::connectOverMMTP()
+{
+    return MmtpMessage();
+}
+
+MmtpMessage CMMSAgent::connectAnonymousOverMMTP()
+{
+    return MmtpMessage();
+}
+
+MmtpMessage CMMSAgent::disconnectOverMMTP()
+{
+    return MmtpMessage();
+}
+
+MmtpMessage CMMSAgent::readMessage(ix::WebSocket& websocket)
+{
+    return MmtpMessage();
+}
+
+void CMMSAgent::writeMessage(ix::WebSocket& websocket)
+{
+}
+
+MmtpMessage CMMSAgent::writeAndReadMessage(ix::WebSocket& websocket)
+{
+    return MmtpMessage();
+}
+
+void CMMSAgent::connectWS(const std::string& url) {
     //std::string url("ws://localhost:8080/");
     webSocket.setUrl(url);
 
     // Optional heart beat, sent every 45 seconds when there is not any traffic
     // to make sure that load balancers do not kill an idle connection.
-    webSocket.setPingInterval(45);
+    webSocket.setPingInterval(60);
 
     // Per message deflate connection is enabled by default. You can tweak its parameters or disable it
     webSocket.disablePerMessageDeflate();
@@ -38,20 +116,4 @@ bool CMMSAgent::connect(const std::string& url) {
 
     // Now that our callback is setup, we can start our background thread and receive messages
     webSocket.start();
-
-    return true;
 }
-
-void CMMSAgent::send(const std::string& mrn, const std::vector<uint8_t>& data) {
-    // Send a message to the server (default to TEXT mode)
-    webSocket.send("hello world");
-
-    // The message can be sent in BINARY mode (useful if you send MsgPack data for example)
-    webSocket.sendBinary("some serialized binary data");
-}
-
-std::vector<uint8_t> CMMSAgent::receive() {
-    std::vector<uint8_t> emptyData;
-    return emptyData;
-}
-
--- OpenS100/MmsAgent.h
+++ OpenS100/MmsAgent.h
@@ -3,6 +3,7 @@
 #include <string>
 #include <vector>
 #include <ixwebsocket/IXWebSocket.h>
+#include "./mmtp.pb.h"
 
 enum class AgentState {
     NOTCONNECTED = 0,
@@ -12,20 +13,45 @@ enum class AgentState {
 
 class CMMSAgent {
 public:
+    std::string Mrn;
+    std::vector<std::string> Interests;
+    // Assuming MmtpMessage is a struct or class that exists in your codebase.
+    std::map<std::string, MmtpMessage*> Messages;
+
+    std::string reconnectToken;
+
+    std::string m_url;
+    // Our websocket object
+    
+
+private:
+    AgentState state;
+
+    // Assuming websocket.Conn is a valid type in your codebase.
+    ix::WebSocket webSocket;
+
+public:
     CMMSAgent();
     ~CMMSAgent();
 
-    bool connect(const std::string& url);
-    bool disconnect();
-    void send(const std::string& mrn, const std::vector<uint8_t>& data);
-    std::vector<uint8_t> receive();
+    ResponseEnum connectAuthenticated(const std::string& url);
+    AgentState status();
+    ResponseEnum connectAnonymous(const std::string& url);
+    ResponseEnum authenticate();
+    ResponseEnum disconnect();
+    ResponseEnum send(const std::string& mrnOrSubject, const bool isBroadcast, const std::vector<uint8_t>& data);
+    ResponseEnum receive();
+    ResponseEnum subscribe(const std::string& subject);
+    ResponseEnum unsubscribe(const std::string& message);
+
 
 private:
-    void onMessage(const std::string& message);
-
-private:
-    std::string m_url;
-    int m_port;
-    // Our websocket object
-    ix::WebSocket webSocket;
+    MmtpMessage readMessage(ix::WebSocket& websocket);
+    void writeMessage(ix::WebSocket& websocket);
+    MmtpMessage writeAndReadMessage(ix::WebSocket& websocket);
+    void connectWS(const std::string& url);
+    void disconnectWS();
+    MmtpMessage connectOverMMTP();
+    MmtpMessage connectAnonymousOverMMTP();
+    MmtpMessage disconnectOverMMTP();
 };
